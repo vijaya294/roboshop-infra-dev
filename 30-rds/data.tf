d603af72a06b99f4fd9482871f7aa0fedb21ce87
Srilatha <koduri0311@gmail.com>
2024-12-17 20:39:30 +0000

    roboshop

---
+++ 30-rds/data.tf
@@ -0,0 +1,14 @@
+data "aws_ssm_parameter" "mysql_sg_id" {
+  #/roboshop/dev/bastion_sg_id
+  name = "/${var.project_name}/${var.environment}/mysql_sg_id"
+}
+
+data "aws_ssm_parameter" "database_subnet_group_name" {
+  name = "/${var.project_name}/${var.environment}/database_subnet_group_name"
+}
+
+
+# data "aws_ssm_parameter" "public_subnet_ids" {
+#   #/roboshop/dev/public_subnet_ids
+#   name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+# }
