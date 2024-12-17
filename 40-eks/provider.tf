dff2d420de996a530b0c9c3e76d45dadfd04b790
Srilatha <koduri0311@gmail.com>
2024-12-17 20:52:16 +0000

    roboshop

---
+++ 40-eks/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.75.0"
+    }
+  }
+
+  
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
