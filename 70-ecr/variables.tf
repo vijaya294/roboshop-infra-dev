cfaf36f89067b299976389af8614dbf0b6dd186d
Srilatha <koduri0311@gmail.com>
2024-12-18 12:56:05 +0000

    roboshop

---
+++ 70-ecr/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "roboshop"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
